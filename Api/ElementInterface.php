c03e743e4e46721c240038e2d50a4b2e43c8bcac
Benjamin Rosenberger <rosenberger@e-conomix.at>
2020-08-24 13:03:05 +0200

    :bug: add missing file to git

---
+++ Api/ElementInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author      Benjamin Rosenberger <rosenberger@e-conomix.at>
+ * @package BroCode\EntityServices\Api
+ * @copyright Copyright (c) 2020 E-CONOMIX GmbH (https://www.e-conomix.at)
+ * @created 24.08.2020
+ */
+
+namespace BroCode\EntityServices\Api;
+
+/**
+ * Interface ElementInterface
+ * @package BroCode\EntityServices\Api
+ */
+interface ElementInterface
+{
+    public function build();
+}
