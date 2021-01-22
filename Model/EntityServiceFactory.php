1951ff3f942dc77a1404888926a7a9fd723cdf26
Benjamin Rosenberger <rosenberger@e-conomix.at>
2021-01-22 07:18:57 +0100

    :sparkles: add salesentity attribute updates

--- Model/EntityServiceFactory.php
+++ Model/EntityServiceFactory.php
@@ -7,6 +7,7 @@
 
 namespace BroCode\EntityServices\Model;
 
+use BroCode\EntityServices\Model\Schema\SalesEntityAttributeElement;
 use Magento\Customer\Model\ResourceModel\Attribute;
 use Magento\Eav\Api\AttributeRepositoryInterface;
 
@@ -28,6 +29,14 @@ class EntityServiceFactory
      * @var \Magento\Eav\Setup\EavSetupFactory
      */
     private $eavSetupFactory;
+    /**
+     * @var \Magento\Sales\Setup\SalesSetupFactory
+     */
+    private $salesSetupFactory;
+    /**
+     * @var \Magento\Quote\Setup\QuoteSetupFactory
+     */
+    private $quoteSetupFactory;
 
     /**
      * EntityServiceFactory constructor.
@@ -37,11 +46,15 @@ class EntityServiceFactory
     public function __construct(
         Attribute $attributeResourceModel,
         AttributeRepositoryInterface $attributeRepository,
-        \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory
+        \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory,
+        \Magento\Sales\Setup\SalesSetupFactory $salesSetupFactory,
+        \Magento\Quote\Setup\QuoteSetupFactory $quoteSetupFactory
     ) {
         $this->attributeResourceModel = $attributeResourceModel;
         $this->attributeRepository = $attributeRepository;
         $this->eavSetupFactory = $eavSetupFactory;
+        $this->salesSetupFactory = $salesSetupFactory;
+        $this->quoteSetupFactory = $quoteSetupFactory;
     }
 
     /**
@@ -77,5 +90,10 @@ class EntityServiceFactory
     public function versionLessThan($context, $versionToBe)
     {
         return version_compare($context->getVersion(), $versionToBe) < 0;
+    }
+
+    public function createSalesEntityAttribute(\Magento\Framework\Setup\ModuleDataSetupInterface $setup)
+    {
+        return new SalesEntityAttributeElement($setup, $this->salesSetupFactory, $this->quoteSetupFactory, $this);
     }
 }
---
+++ Model/Schema/SalesEntityAttributeElement.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * @author      Benjamin Rosenberger <rosenberger@e-conomix.at>
+ * @package BroCode\EntityServices\Model\Schema
+ * @copyright Copyright (c) 2021 E-CONOMIX GmbH (https://www.e-conomix.at)
+ * @created 18.01.2021
+ */
+
+namespace BroCode\EntityServices\Model\Schema;
+
+use BroCode\EntityServices\Api\ElementInterface;
+
+/**
+ * Class SalesEntityAttributeElement
+ * @package BroCode\EntityServices\Model\Schema
+ */
+class SalesEntityAttributeElement implements ElementInterface
+{
+    /**
+     * @var ElementInterface
+     */
+    protected $parent;
+    /**
+     * @var \Magento\Sales\Setup\SalesSetup
+     */
+    protected $salesSetup = null;
+    /**
+     * @var \Magento\Sales\Setup\SalesSetupFactory
+     */
+    protected $salesSetupFactory;
+    /**
+     * @var \Magento\Quote\Setup\QuoteSetup
+     */
+    protected $quoteSetup;
+    /**
+     * @var \Magento\Quote\Setup\QuoteSetupFactory
+     */
+    protected $quoteSetupFactory;
+    /**
+     * @var \Magento\Framework\Setup\ModuleDataSetupInterface
+     */
+    protected $setup;
+
+    /**
+     * SalesEntityAttributeElement constructor.
+     * @param $parent
+     */
+    public function __construct(
+        \Magento\Framework\Setup\ModuleDataSetupInterface $setup,
+        \Magento\Sales\Setup\SalesSetupFactory $salesSetupFactory,
+        \Magento\Quote\Setup\QuoteSetupFactory $quoteSetupFactory,
+        $parent
+    ) {
+        $this->parent = $parent;
+        $this->salesSetupFactory = $salesSetupFactory;
+        $this->quoteSetupFactory = $quoteSetupFactory;
+        $this->setup = $setup;
+    }
+
+    public function withQuoteAttribute($attributeCode, array $columnOptions)
+    {
+        $this->getQuoteSetup()->addAttribute('quote', $attributeCode, $columnOptions);
+    }
+
+    public function withOrderAttribute($attributeCode, array $columnOptions)
+    {
+        return $this->withSalesEntityAttribute('order', $attributeCode, $columnOptions);
+    }
+
+    public function withInvoiceAttribute($attributeCode, array $columnOptions)
+    {
+        return $this->withSalesEntityAttribute('invoice', $attributeCode, $columnOptions);
+    }
+
+    public function withSalesEntityAttribute($entityType, $attributeCode, array $columnOptions)
+    {
+        $this->getSalesSetup()->addAttribute($entityType, $attributeCode, $columnOptions);
+        return $this;
+    }
+
+    public function build()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @return \Magento\Sales\Setup\SalesSetup
+     */
+    protected function getQuoteSetup() {
+        if ($this->quoteSetup == null) {
+            $this->quoteSetup = $this->quoteSetupFactory->create(['resourceName' => 'quote_setup', 'setup' => $this->setup]);
+        }
+        return $this->quoteSetup;
+    }
+    /**
+     * @return \Magento\Sales\Setup\SalesSetup
+     */
+    protected function getSalesSetup() {
+        if ($this->salesSetup == null) {
+            $this->salesSetup = $this->salesSetupFactory->create(['resourceName' => 'sales_setup', 'setup' => $this->setup]);
+        }
+        return $this->salesSetup;
+    }
+}
